b0f37fe9abdb6af3abd6615198e4871cf2436558
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-12-10 15:38:50 +0100

    Initial commit

---
+++ game-of-chance-simulator/ILogger.cs
@@ -0,0 +1,8 @@
+namespace GameOfChanceSimulator
+{
+    interface ILogger
+    {
+        void Info(string message);
+        void Error(string message);
+    }
+}
---
+++ game-of-chance-simulator/Program.cs
@@ -0,0 +1,9 @@
+namespace GameOfChanceSimulator
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
