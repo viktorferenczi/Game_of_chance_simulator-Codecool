15195e1782f3f524219f534e5c9c964e95180923
viktorferenczi <ferencziviktor11@gmail.com>
2020-01-06 14:05:27 +0100

    adding

---
+++ game-of-chance-simulator/GameSimulator.cs
@@ -0,0 +1,10 @@
+using System;
+namespace GameOfChanceSimulator
+{
+    public class GameSimulator
+    {
+        public GameSimulator()
+        {
+        }
+    }
+}
--- game-of-chance-simulator/Turtle.cs
+++ game-of-chance-simulator/Turtle.cs
@@ -3,8 +3,16 @@ namespace GameOfChanceSimulator
 {
     public class Turtle
     {
-        public Turtle()
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public float Crit { get; set; }
+
+        public Turtle(int health,int damage,float crit)
         {
+            this.Health = health;
+            this.Damage = damage;
+            this.Crit = crit;
         }
+
     }
 }
